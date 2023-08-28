c00c0b3651582b769fd4ab0e319801710cc9b8a6
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2023-08-28 10:48:20 +0700

    initial commit

---
+++ back-end/app.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+// 404 handler
+app.use((req, res, next) => {
+  return res.status(404).json({ message: `can't find ${req.url}` });
+});
+
+// 500 handler
+app.use((err, req, res, next) => {
+  return res.status(500).json({ message: err.message });
+});
+
+module.exports = app;
